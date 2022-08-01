99142d09d006ff23c961e05fb0a370afff88f388
Jens Neuerburg <jens.neuerburg@hiz-saarland.de>
2022-08-01 08:50:41 +0200

    add buffer navigation for bufferline

--- lua/user/keymaps.lua
+++ lua/user/keymaps.lua
@@ -19,6 +19,10 @@ keymap("n", "<C-j>", "<C-w>j", opts)
 keymap("n", "<C-k>", "<C-w>k", opts)
 keymap("n", "<C-l>", "<C-w>l", opts)
 
+-- Buffer navigation
+keymap("n", "<S-l>", ":bnext<CR>", opts) 
+keymap("n", "<S-h>", ":bprevious<CR>", opts) 
+
 -- Split windows vertically and horizontally
 keymap("n", "<leader>wsv", ":vsplit<CR>", opts)
 keymap("n", "<leader>wsh", ":split<CR>", opts)
