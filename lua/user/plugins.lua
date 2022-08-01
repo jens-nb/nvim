99101f48dd13f0e6a50e1b2506e6dc4783d70a58
Jens Neuerburg <jens.neuerburg@hiz-saarland.de>
2022-08-01 08:49:59 +0200

    add bufferline and devicons

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -44,7 +44,9 @@ return packer.startup(function(use)
   use { "wbthomason/packer.nvim", commit = "00ec5adef58c5ff9a07f11f45903b9dbbaa1b422" } -- Have packer manage itself
   use { "nvim-lua/plenary.nvim", commit = "968a4b9afec0c633bc369662e78f8c5db0eba249" } -- Useful lua functions used by lots of plugins
   use { "williamboman/mason.nvim", commit = "2680ff415265665de7c88b766818e34870f522c3" }
-	--
+	use	{	"https://github.com/akinsho/bufferline.nvim", commit = "c4dd9b4de03b891f648b098c25e4dc1bc48a13e5"}
+	use { "kyazdani42/nvim-web-devicons", commit = "2d02a56189e2bde11edd4712fea16f08a6656944" }
+
   -- Colorschemes
   use { "folke/tokyonight.nvim", commit = "8223c970677e4d88c9b6b6d81bda23daf11062bb" }
   use { "lunarvim/darkplus.nvim", commit = "2584cdeefc078351a79073322eb7f14d7fbb1835" }
