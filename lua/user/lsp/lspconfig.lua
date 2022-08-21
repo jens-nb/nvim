b1f8c6ea83b639a1489864880b584863da24e015
Jens Neuerburg <jens.neuerburg@hiz-saarland.de>
2022-08-21 22:40:48 +0200

    add gopls

--- lua/user/lsp/lspconfig.lua
+++ lua/user/lsp/lspconfig.lua
@@ -37,20 +37,32 @@ local on_attach = function(client, bufnr)
   vim.keymap.set('n', '<space>f', vim.lsp.buf.formatting, bufopts)
 end
 
+-- CMP
+local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
+
 local lsp_flags = {
   -- This is the default in Nvim 0.7+
   debounce_text_changes = 150,
 }
+
 lspconfig.pyright.setup{
     on_attach = on_attach,
     flags = lsp_flags,
+		capabilities = capabilities
 }
+
 lspconfig.bashls.setup{
     on_attach = on_attach,
     flags = lsp_flags,
+		capabilities = capabilities
 }
 
-local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
+lspconfig.gopls.setup{
+    on_attach = on_attach,
+    flags = lsp_flags,
+		capabilities = capabilities
+}
+
 lspconfig.sumneko_lua.setup{
     on_attach = on_attach,
     flags = lsp_flags,
